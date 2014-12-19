111d4bb56effba12d047b315092ba64c37db3d19
Wouter Coekaerts <wouter@squareup.com>
2014-12-18 23:33:41 -0800

    Add tracker for System.out/err.charOut

--- flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/hook/SystemHook.java
+++ flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/hook/SystemHook.java
@@ -2,6 +2,8 @@ package be.coekaerts.wouter.flowtracker.hook;
 
 import be.coekaerts.wouter.flowtracker.tracker.Tracker;
 import be.coekaerts.wouter.flowtracker.tracker.TrackerRepository;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
 
 @SuppressWarnings("UnusedDeclaration") // used by instrumented code
 public class SystemHook {
@@ -12,8 +14,26 @@ public class SystemHook {
 	}
 
   public static void initialize() {
+    // not really used
     TrackerRepository.createContentTracker(System.out).initDescriptor("System.out", null);
     TrackerRepository.createContentTracker(System.err).initDescriptor("System.err", null);
     TrackerRepository.createContentTracker(System.in).initDescriptor("System.in", null);
+
+    // we have to add trackers to the existing streams, because they were created before flowtracker
+    // was initialized.
+    addSystemOutErrTracker(System.out, "System.out");
+    addSystemOutErrTracker(System.err, "System.err");
+  }
+
+  private static void addSystemOutErrTracker(PrintStream printStream, String name) {
+    try {
+      Field charOutField = PrintStream.class.getDeclaredField("charOut");
+      charOutField.setAccessible(true);
+      TrackerRepository.createContentTracker(charOutField.get(printStream))
+          .initDescriptor(name + ".charOut", null);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      System.err.println("Cannot access PrintStream.charOut. Cannot hook System.out/err:");
+      e.printStackTrace(System.err);
+    }
   }
 }
