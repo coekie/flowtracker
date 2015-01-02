dcf80234b74bd1b7e4b94e44b1b1c55f511f0b6b
Wouter Coekaerts <wouter@squareup.com>
2015-01-01 23:21:36 -0800

    Test jar URLs

--- flowtracker-test/src/test/java/be/coekaerts/wouter/flowtracker/test/URLTest.java
+++ flowtracker-test/src/test/java/be/coekaerts/wouter/flowtracker/test/URLTest.java
@@ -9,12 +9,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class URLTest {
-  @Test public void openStream() throws IOException {
+  /**
+   * Test getting an input stream from a file in the filesystem. We use our own class file for
+   * convenience.
+   */
+  @Test public void fileURLConnection() throws IOException {
     URL url = URLTest.class.getResource("URLTest.class");
     InputStream in = url.openStream();
     String descriptor = TrackerRepository.getTracker(in).getDescriptor();
-    assertTrue(descriptor.startsWith("InputStream from"));
+    assertTrue(descriptor.startsWith("InputStream from file:"));
     assertTrue(descriptor.endsWith(URLTest.class.getName().replace(".", "/") + ".class"));
     in.close();
+  }
+
+  /** Test getting an input stream from a jar file */
+  @Test public void jarURLConnection() throws IOException {
+    URL url = String.class.getResource("String.class");
+    InputStream in = url.openStream();
+    String descriptor = TrackerRepository.getTracker(in).getDescriptor();
+    assertTrue(descriptor.startsWith("InputStream from jar:file:"));
+    assertTrue(descriptor.endsWith(".jar!/java/lang/String.class"));
+    System.out.println(TrackerRepository.getTracker(in).getDescriptor());
   }
 }
