85a9b211ff3017e5bc37fe34b32088b319d6f603
Wouter Coekaerts <wouter@coekaerts.be>
2023-01-30 00:13:24 +0100

    Fix recursion in initialization of ConcurrentSkipListMap

--- flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/CoreInitializer.java
+++ flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/CoreInitializer.java
@@ -4,12 +4,20 @@ import be.coekaerts.wouter.flowtracker.hook.StringHook;
 import be.coekaerts.wouter.flowtracker.hook.SystemHook;
 import be.coekaerts.wouter.flowtracker.util.ShutdownSuspender;
 import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 @SuppressWarnings("UnusedDeclaration") // called with reflection from FlowTrackAgent
 public class CoreInitializer {
   public static void initialize(Map<String, String> config) {
+    ensureInitialized();
     SystemHook.initialize();
     StringHook.initDebugUntracked(config.get(StringHook.DEBUG_UNTRACKED));
+  }
+
+  // call stuff to make sure JDK internals needed for it are initialized, before we enable tracking
+  @SuppressWarnings("WriteOnlyObject")
+  private static void ensureInitialized() {
+    new ConcurrentSkipListMap<Integer, Integer>().put(1, 1);
   }
 
   /**
