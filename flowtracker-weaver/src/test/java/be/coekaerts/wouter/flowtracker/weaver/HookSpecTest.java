889c2bf58b20caecd76870629189ba7342cf8e9d
Wouter Coekaerts <wouter@coekaerts.be>
2024-01-20 18:39:36 +0100

    Factor out TransformerTestUtils

--- flowtracker-weaver/src/test/java/be/coekaerts/wouter/flowtracker/weaver/HookSpecTest.java
+++ flowtracker-weaver/src/test/java/be/coekaerts/wouter/flowtracker/weaver/HookSpecTest.java
@@ -9,24 +9,12 @@ import be.coekaerts.wouter.flowtracker.tracker.Invocation;
 import be.coekaerts.wouter.flowtracker.tracker.TrackerPoint;
 import be.coekaerts.wouter.flowtracker.weaver.HookSpec.HookArgument;
 import be.coekaerts.wouter.flowtracker.weaver.HookSpec.OnEnterHookArgumentInstance;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.lang.reflect.Constructor;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Type;
-import org.objectweb.asm.commons.ClassRemapper;
 import org.objectweb.asm.commons.GeneratorAdapter;
-import org.objectweb.asm.commons.SimpleRemapper;
-import org.objectweb.asm.util.CheckClassAdapter;
-import org.objectweb.asm.util.TraceClassVisitor;
 
 public class HookSpecTest {
   public static StringBuilder log;
@@ -112,75 +100,8 @@ public class HookSpecTest {
    */
   private void transformAndRun(ClassHookSpec classHookSpec, Object... constructArgs)
       throws ReflectiveOperationException {
-    Class<?> clazz = transformClass(classHookSpec);
-    Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
-    constructor.setAccessible(true);
-    Object instance = constructor.newInstance(constructArgs);
-    ((Runnable) instance).run();
-  }
-
-  private static Class<?> transformClass(ClassHookSpec classHookSpec) {
-    String className = classHookSpec.getTargetClass().getClassName();
-    String suffix = "2"; // transformed class named className + suffix
-
-    ClassWriter classWriter = new ClassWriter(0);
-    StringWriter verifyStringWriter = new StringWriter();
-    PrintWriter verifyPrintWriter = new PrintWriter(verifyStringWriter);
-    StringWriter afterStringWriter = new StringWriter();
-    PrintWriter afterPrintWriter = new PrintWriter(afterStringWriter);
-    StringWriter beforeStringWriter = new StringWriter();
-    PrintWriter beforePrintWriter = new PrintWriter(beforeStringWriter);
-
-    // writes out bytecode to text after transformation
-    TraceClassVisitor afterVisitor =
-        new TraceClassVisitor(new CheckClassAdapter(classWriter), afterPrintWriter);
-    ClassVisitor transformingVisitor =
-        classHookSpec.createClassAdapter(className, afterVisitor);
-    // writes out original bytecode to text
-    TraceClassVisitor beforeVisitor =
-        new TraceClassVisitor(transformingVisitor, beforePrintWriter);
-    ClassRemapper remapper = new ClassRemapper(beforeVisitor,
-        new SimpleRemapper(Map.of(classHookSpec.getTargetClass().getInternalName(),
-            classHookSpec.getTargetClass().getInternalName() + suffix)));
-
-    try {
-      new ClassReader(className)
-          .accept(remapper, ClassReader.EXPAND_FRAMES | ClassReader.SKIP_DEBUG);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-
-    // verify bytecode using asm. this is not as thorough as the jvm, but gives more helpful error
-    // messages when it fails
-    CheckClassAdapter.verify(new ClassReader(classWriter.toByteArray()), false, verifyPrintWriter);
-    assertEquals("", verifyStringWriter.toString());
-
-    try {
-      return loadClass(className + suffix, classWriter.toByteArray());
-    } catch (VerifyError e) {
-      throw new AssertionError("Verification failed. Original code:\n" + beforeStringWriter
-          + "\nTransformed code:\n" + afterStringWriter, e);
-    }
-  }
-
-  /**
-   * Load a class in a child classloader.
-   */
-  private static Class<?> loadClass(String className, byte[] byteCode) {
-    ClassLoader classLoader = new ClassLoader(HookSpecTest.class.getClassLoader()) {
-      @Override
-      protected Class<?> findClass(String name) throws ClassNotFoundException {
-        if (name.equals(className)) {
-          return defineClass(name, byteCode, 0, byteCode.length);
-        }
-        return super.findClass(name);
-      }
-    };
-    try {
-      return classLoader.loadClass(className);
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    TransformerTestUtils.transformAndRun(classHookSpec, classHookSpec.getTargetClass(),
+        constructArgs);
   }
 
   public static void log(Object... strs) {
