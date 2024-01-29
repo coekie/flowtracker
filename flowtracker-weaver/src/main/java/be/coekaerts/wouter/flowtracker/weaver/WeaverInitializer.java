78daf0f490c4800c676add61f809583e46f67a60
Wouter Coekaerts <wouter@coekaerts.be>
2024-01-29 16:20:09 +0100

    Retransform classes at startup together
    This speeds up startup.

--- flowtracker-weaver/src/main/java/be/coekaerts/wouter/flowtracker/weaver/WeaverInitializer.java
+++ flowtracker-weaver/src/main/java/be/coekaerts/wouter/flowtracker/weaver/WeaverInitializer.java
@@ -1,24 +1,35 @@
 package be.coekaerts.wouter.flowtracker.weaver;
 
+import be.coekaerts.wouter.flowtracker.hook.ArrayLoadHook;
+import be.coekaerts.wouter.flowtracker.tracker.TrackerRepository;
 import java.lang.instrument.Instrumentation;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("UnusedDeclaration") // called with reflection from FlowTrackAgent
 public class WeaverInitializer {
   public static void initialize(Instrumentation inst, Map<String, String> config) throws Exception {
+    // avoid ClassCircularityErrors: Make sure these hook classes are loaded before we start
+    // transforming
+    ArrayLoadHook.class.getName();
+    TrackerRepository.class.getName();
+
     AsmTransformer transformer = new AsmTransformer(config);
     inst.addTransformer(transformer, true);
 
     // retransform classes that have already been loaded
+    List<Class> toTransform = new ArrayList<>();
     for (Class<?> loadedClass : inst.getAllLoadedClasses()) {
       if (transformer.shouldRetransformOnStartup(loadedClass, inst)) {
-        try {
-          inst.retransformClasses(loadedClass);
-        } catch (Throwable t) {
-          System.err.println("Failed to retransform " + loadedClass);
-          throw t;
-        }
+        toTransform.add(loadedClass);
       }
+    }
+    try {
+      inst.retransformClasses(toTransform.toArray(new Class[0]));
+    } catch (Throwable t) {
+      System.err.println("Failed to retransform");
+      throw t;
     }
   }
 }
