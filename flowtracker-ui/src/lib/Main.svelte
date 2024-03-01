7bb17a52336072699bb359d0bf1536f226f2e093
Wouter Coekaerts <wouter@coekaerts.be>
2024-03-01 11:18:29 +0100

    Use svelte-splitpanes

--- flowtracker-ui/src/lib/Main.svelte
+++ flowtracker-ui/src/lib/Main.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Pane, Splitpanes } from 'svelte-splitpanes';
+
   import SettingsView from './SettingsView.svelte'
   import TrackerDetailView from './TrackerDetailView.svelte'
   import type { Tracker } from '../javatypes'
@@ -12,46 +14,23 @@
   let selection: SelectedRange | null;
 </script>
 
-<div class="wrapper">
-  <div class="left">
+<Splitpanes theme="my-theme">
+	<Pane>
     <TrackerTree bind:selectedTracker={mainTracker}/>
-  </div>
-  <div class="right">
-    <div class="updown">
-      <TrackerDetailView bind:viewTracker={mainTracker} bind:selection={selection}/>
-    </div>
-    <div class="updown">
-      <TrackerDetailView viewTracker={selection ? selection.tracker : null} bind:selection={selection} targetTracker={mainTracker}/>
-    </div>
-  </div>
-</div>
+	</Pane>
+	<Pane>
+    <Splitpanes horizontal={true} theme="my-theme" >
+      <Pane>
+        <TrackerDetailView bind:viewTracker={mainTracker} bind:selection={selection}/>
+      </Pane>
+      <Pane>
+        <TrackerDetailView viewTracker={selection ? selection.tracker : null} bind:selection={selection} targetTracker={mainTracker}/>
+      </Pane>
+    </Splitpanes>
+	</Pane>
+</Splitpanes>
+
 <SettingsView />
 
 <style>
-  .wrapper {
-    position: absolute;
-    display: flex;
-    top: 0;
-    bottom: 50px;
-    width: 100%;
-  }
-
-  .left {
-    flex-basis: 30%;
-    flex-grow: 1;
-    overflow: hidden;
-  }
-
-  .right {
-    flex-basis: 70%;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    overflow: hidden;
-  }
-
-  .updown {
-    flex-basis: 50%;
-    overflow: hidden;
-  }
 </style>
