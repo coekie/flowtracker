e3e2ecdd1298881ac3a7449cd2a100e9f207db3c
Wouter Coekaerts <wouter@coekaerts.be>
2024-03-19 18:18:01 +0100

    Add eslint-config-prettier
    "Turns off all rules that are unnecessary or might conflict with
    Prettier."

--- flowtracker-ui/.eslintrc.cjs
+++ flowtracker-ui/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:svelte/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
