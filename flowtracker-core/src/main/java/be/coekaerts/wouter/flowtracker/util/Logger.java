c0d318fb264f811b2a719c55305051a140372c2f
Wouter Coekaerts <wouter@coekaerts.be>
2024-04-22 23:46:23 +0200

    Add exitOnError, and enable it in tests
    This currently makes the flowtracker-test-full tests fail, as they
    should.

--- flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/util/Logger.java
+++ flowtracker-core/src/main/java/be/coekaerts/wouter/flowtracker/util/Logger.java
@@ -2,11 +2,17 @@ package be.coekaerts.wouter.flowtracker.util;
 
 import be.coekaerts.wouter.flowtracker.tracker.Trackers;
 
+/**
+ * Handles logging for flowtracker. Deliberately very primitive, not using a _proper_ logging
+ * framework.
+ */
 public class Logger {
   private static boolean logging;
+  private static boolean exitOnError;
 
   public static void initLogging(Config config) {
     logging = config.getBoolean("logging", false);
+    exitOnError = config.getBoolean("exitOnError", false);
   }
 
   private final String name;
@@ -33,13 +39,18 @@ public class Logger {
     try {
       System.err.println("[FT " + name + " ERROR] " + String.format(format, args));
       if (t != null) {
+        //noinspection CallToPrintStackTrace
         t.printStackTrace();
+      }
+      if (exitOnError) {
+        // forcefully exit, don't even try to run shutdown hooks. this avoids stuff going crazy if
+        // our instrumentation has caused something fundamental to fail.
+        Runtime.getRuntime().halt(1);
       }
     } finally {
       Trackers.unsuspendOnCurrentThread();
     }
   }
-
 
   public void error(String format, Object... args) {
     error(null, format, args);
