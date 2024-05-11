3bc63d6b8d40c2f7817e10b130769a47d86c5d04
Wouter Coekaerts <wouter@coekaerts.be>
2024-05-12 00:38:05 +0200

    Add license headers on source files

--- flowtracker-core/src/main/java/com/coekie/flowtracker/tracker/TrackerDepth.java
+++ flowtracker-core/src/main/java/com/coekie/flowtracker/tracker/TrackerDepth.java
@@ -1,5 +1,21 @@
 package com.coekie.flowtracker.tracker;
 
+/*-
+ * Copyright 2024 Wouter Coekaerts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /** Specifies how deep to look for the source of a value. */
 // TODO the only depths we have are CONTENT_IMMUTABLE and ORIGIN, and in practice they are the same.
 // While e.g. String is immutable, we are just tracking the content of it which is an array which as
