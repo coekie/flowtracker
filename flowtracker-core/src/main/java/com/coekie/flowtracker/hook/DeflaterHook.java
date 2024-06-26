c4e69722c678096f2e4ac47170f4f7d505050e2d
Wouter Coekaerts <wouter@coekaerts.be>
2024-06-26 12:42:10 +0200

    Track input and output of Deflater

---
+++ flowtracker-core/src/main/java/com/coekie/flowtracker/hook/DeflaterHook.java
@@ -0,0 +1,149 @@
+package com.coekie.flowtracker.hook;
+
+/*-
+ * Copyright 2024 Wouter Coekaerts
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static com.coekie.flowtracker.tracker.Context.context;
+
+import com.coekie.flowtracker.annotation.Arg;
+import com.coekie.flowtracker.annotation.Hook;
+import com.coekie.flowtracker.annotation.HookLocation;
+import com.coekie.flowtracker.tracker.ByteOriginTracker;
+import com.coekie.flowtracker.tracker.ByteSinkTracker;
+import com.coekie.flowtracker.tracker.Context;
+import com.coekie.flowtracker.tracker.TrackerUpdater;
+import com.coekie.flowtracker.util.ConcurrentWeakIdentityHashMap;
+import java.lang.invoke.VarHandle;
+import java.nio.ByteBuffer;
+import java.util.zip.Deflater;
+
+/**
+ * Tracks the input going into a {@link Deflater} and its output, in two separate trackers.
+ * No attempt is made to track exactly which uncompressed bytes particular compressed bytes
+ * correspond to.
+ */
+public class DeflaterHook {
+  private static final ConcurrentWeakIdentityHashMap<Deflater, DeflaterState> stateMap
+      = new ConcurrentWeakIdentityHashMap<>();
+
+  private static final VarHandle deflater_input =
+      Reflection.varHandle(Deflater.class, "input", ByteBuffer.class);
+  private static final VarHandle deflater_inputArray =
+      Reflection.varHandle(Deflater.class, "inputArray", byte[].class);
+  private static final VarHandle deflater_inputPos =
+      Reflection.varHandle(Deflater.class, "inputPos", int.class);
+
+  @Hook(
+      target = "java/util/zip/Deflater",
+      method = "int deflate(byte[], int, int, int)",
+      location = HookLocation.ON_ENTER)
+  @Hook(
+      target = "java/util/zip/Deflater",
+      method = "int deflate(java.nio.ByteBuffer, int)",
+      location = HookLocation.ON_ENTER)
+  public static void beforeDeflate(@Arg("THIS") Deflater deflater) {
+    // remember where we were in the input before deflate was called, so that we know afterward
+    // how much of the input was consumed
+    DeflaterState state = stateMap.computeIfAbsent(deflater, d -> new DeflaterState());
+    ByteBuffer input = (ByteBuffer) deflater_input.get(deflater);
+    if (input != null) {
+      state.inputPosBeforeDeflate = input.position();
+    } else {
+      state.inputPosBeforeDeflate = (int) deflater_inputPos.get(deflater);
+    }
+  }
+
+  @Hook(
+      target = "java/util/zip/Deflater",
+      method = "int deflate(byte[], int, int, int)",
+      location = HookLocation.ON_RETURN)
+  public static void afterDeflateBytes(@Arg("RETURN") int written, @Arg("THIS") Deflater deflater,
+      @Arg("ARG0") byte[] output, @Arg("ARG1") int outputOffset) {
+    DeflaterState state = stateMap.get(deflater);
+    Context context = context();
+    updateSinkAfterDeflate(context, deflater, state);
+    updateOriginAfterDeflate(context, state, output, outputOffset, written);
+  }
+
+  @Hook(
+      target = "java/util/zip/Deflater",
+      method = "int deflate(java.nio.ByteBuffer, int)",
+      location = HookLocation.ON_RETURN)
+  public static void afterDeflateBuffer(@Arg("RETURN") int written, @Arg("THIS") Deflater deflater,
+      @Arg("ARG0") ByteBuffer outputBuffer) {
+    DeflaterState state = stateMap.get(deflater);
+    Context context = context();
+    updateSinkAfterDeflate(context, deflater, state);
+
+    if (!outputBuffer.isDirect()) {
+      byte[] output = ByteBufferHook.hb(outputBuffer);
+      int outputOffset = ByteBufferHook.offset(outputBuffer) + outputBuffer.position() - written;
+      updateOriginAfterDeflate(context, state, output, outputOffset, written);
+    }
+  }
+
+  /**
+   * Update the sink tracker, tracking the input that was deflated
+   */
+  private static void updateSinkAfterDeflate(Context context, Deflater deflater,
+      DeflaterState state) {
+    if (state.sinkTracker == null) {
+      state.sinkTracker = new ByteSinkTracker();
+    }
+
+    ByteBuffer input = (ByteBuffer) deflater_input.get(deflater);
+    if (input != null) {
+      TrackerUpdater.appendByteBuffer(context, state.sinkTracker, input,
+          state.inputPosBeforeDeflate, input.position() - state.inputPosBeforeDeflate);
+    } else {
+      byte[] inputArray = (byte[]) deflater_inputArray.get(deflater);
+      int pos = (int) deflater_inputPos.get(deflater);
+      TrackerUpdater.appendBytes(context, state.sinkTracker, inputArray,
+          state.inputPosBeforeDeflate, pos - state.inputPosBeforeDeflate);
+    }
+  }
+
+  /**
+   * Update the origin tracker, tracking the deflated output
+   */
+  private static void updateOriginAfterDeflate(Context context, DeflaterState state, byte[] output,
+      int outputOffset, int written) {
+    if (state.originTracker == null) {
+      state.originTracker = new ByteOriginTracker();
+    }
+    int originTrackerIndex = state.originTracker.getLength();
+    state.originTracker.append(output, outputOffset, written);
+    TrackerUpdater.setSourceTracker(context, output, outputOffset, written, state.originTracker,
+        originTrackerIndex);
+  }
+
+  private static class DeflaterState {
+    /** Position in the input before deflate was called */
+    private int inputPosBeforeDeflate;
+    private ByteSinkTracker sinkTracker;
+    private ByteOriginTracker originTracker;
+  }
+
+  public static ByteSinkTracker getSinkTracker(Deflater deflater) {
+    DeflaterState state = stateMap.get(deflater);
+    return state == null ? null : state.sinkTracker;
+  }
+
+  public static ByteOriginTracker getOriginTracker(Deflater deflater) {
+    DeflaterState state = stateMap.get(deflater);
+    return state == null ? null : state.originTracker;
+  }
+}
---
+++ flowtracker-test/src/test/java/com/coekie/flowtracker/test/DeflaterTest.java
@@ -0,0 +1,109 @@
+package com.coekie.flowtracker.test;
+
+import static com.coekie.flowtracker.tracker.TrackerSnapshot.assertThatTracker;
+import static com.coekie.flowtracker.tracker.TrackerSnapshot.assertThatTrackerOf;
+import static com.coekie.flowtracker.tracker.TrackerSnapshot.snapshot;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.coekie.flowtracker.hook.DeflaterHook;
+import com.coekie.flowtracker.tracker.ByteOriginTracker;
+import com.coekie.flowtracker.tracker.ByteSinkTracker;
+import java.nio.ByteBuffer;
+import java.util.zip.Deflater;
+import org.junit.After;
+import org.junit.Test;
+
+public class DeflaterTest {
+  private final byte[] abc = TrackTestHelper.trackedByteArray("abc");
+  private final byte[] xdefx = TrackTestHelper.trackedByteArray("xxxdefxxxx");
+
+  private Deflater deflater;
+
+  @After
+  public void after() {
+    if (deflater != null) {
+      deflater.end();
+    }
+  }
+
+  @Test
+  public void testArrayToArray() {
+    testDeflateToArray(InputType.ARRAY, OutputType.ARRAY);
+  }
+
+  @Test
+  public void testBufferToArray() {
+    testDeflateToArray(InputType.BYTE_BUFFER, OutputType.ARRAY);
+  }
+
+  @Test
+  public void testArrayToBuffer() {
+    testDeflateToArray(InputType.ARRAY, OutputType.BYTE_BUFFER);
+  }
+
+  @Test
+  public void testBufferToBufffer() {
+    testDeflateToArray(InputType.BYTE_BUFFER, OutputType.BYTE_BUFFER);
+  }
+
+  private void testDeflateToArray(InputType inputType, OutputType outputType) {
+    deflater = new Deflater();
+    byte[] out = new byte[64];
+
+    // set `abc` as input for first deflate call
+    if (inputType == InputType.ARRAY) {
+      deflater.setInput(abc);
+    } else {
+      deflater.setInput(ByteBuffer.wrap(abc));
+    }
+    int result = callDeflate(outputType, deflater, out, 0, 64);
+    assertThat(result).isGreaterThan(0);
+
+    // set `def` as input for second deflate call
+    if (inputType == InputType.ARRAY) {
+      deflater.setInput(xdefx, 3, 3);
+    } else {
+      ByteBuffer buf = ByteBuffer.wrap(xdefx);
+      buf.position(1);
+      buf = buf.slice(); // test with buffer with non-zero offset
+      buf.position(2);
+      buf.limit(5);
+      deflater.setInput(buf);
+    }
+    deflater.finish();
+    result += callDeflate(outputType, deflater, out, result, 64 - result);
+
+    assertThat(deflater.finished()).isTrue();
+
+    ByteSinkTracker sinkTracker = DeflaterHook.getSinkTracker(deflater);
+    assertThatTracker(sinkTracker).matches(snapshot().track(abc).track(3, xdefx, 3));
+
+    ByteOriginTracker originTracker = DeflaterHook.getOriginTracker(deflater);
+    assertThatTrackerOf(out).matches(snapshot().part(result, originTracker, 0));
+  }
+
+  private int callDeflate(OutputType outputType, Deflater deflater, byte[] output, int outputPos,
+      int outputLen) {
+    if (outputType == OutputType.ARRAY) {
+      return deflater.deflate(output, outputPos, outputLen, Deflater.FULL_FLUSH);
+    } else if (outputType == OutputType.BYTE_BUFFER) {
+      ByteBuffer buf = ByteBuffer.wrap(output);
+      buf.position(outputPos);
+      buf.limit(outputPos + outputLen);
+      deflater.deflate(buf, Deflater.FULL_FLUSH);
+      return buf.position() - outputPos;
+    } else {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  enum InputType {
+    ARRAY,
+    BYTE_BUFFER
+  }
+
+  enum OutputType {
+    ARRAY,
+    BYTE_BUFFER
+  }
+}
