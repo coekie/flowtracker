8f99b03a58eb96f1c8729eccca5fe3e6255ad49f
Wouter Coekaerts <wouter@coekaerts.be>
2024-07-05 22:38:35 +0200

    Refactor SSLSocketTest
    Share code between direct and layered connection tests

--- flowtracker-test/src/test/java/com/coekie/flowtracker/test/SSLSocketTest.java
+++ flowtracker-test/src/test/java/com/coekie/flowtracker/test/SSLSocketTest.java
@@ -15,84 +15,63 @@ import org.junit.Test;
  * @see SocketTest
  */
 public class SSLSocketTest {
-  @Test public void testClient() throws IOException {
+  @Test public void testClientDirect() throws IOException {
     try (SSLSocketTester tester = SSLSocketTester.createDirect()) {
-      Tracker readTracker = TrackerRepository.getTracker(context(), tester.client.getInputStream());
-      assertThatTrackerNode(readTracker)
-          .hasPathStartingWith("Client socket")
-          .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
-              "From " + tester.client.getLocalPort(),
-              FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
-      Tracker writeTracker = TrackerRepository.getTracker(context(),
-          tester.client.getOutputStream());
-      assertThatTrackerNode(writeTracker)
-          .hasPathStartingWith("Client socket")
-          .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
-              "From " + tester.client.getLocalPort(),
-              FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
-      assertThat(readTracker.twin).isEqualTo(writeTracker);
-      assertThat(writeTracker.twin).isEqualTo(readTracker);
+      testClient(tester);
     }
   }
 
-  @Test public void testServer() throws IOException {
+  @Test public void testServerDirect() throws IOException {
     try (SSLSocketTester tester = SSLSocketTester.createDirect()) {
-      Tracker readTracker = TrackerRepository.getTracker(context(), tester.server.getInputStream());
-      assertThatTrackerNode(readTracker)
-          .hasPathStartingWith("Server socket")
-          .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
-              tester.server.getRemoteSocketAddress().toString(),
-              FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
-      Tracker writeTracker = TrackerRepository.getTracker(context(),
-          tester.server.getOutputStream());
-      assertThatTrackerNode(writeTracker)
-          .hasPathStartingWith("Server socket")
-          .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
-              tester.server.getRemoteSocketAddress().toString(),
-              FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
-      assertThat(readTracker.twin).isEqualTo(writeTracker);
-      assertThat(writeTracker.twin).isEqualTo(readTracker);
+      testServer(tester);
     }
   }
 
-  // TODO refactor, share code
   @Test public void testClientLayered() throws IOException {
     try (SSLSocketTester tester = SSLSocketTester.createLayered()) {
-      Tracker readTracker = TrackerRepository.getTracker(context(), tester.client.getInputStream());
-      assertThatTrackerNode(readTracker)
-          .hasPathStartingWith("Client socket")
-          .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
-              "From " + tester.client.getLocalPort(),
-              FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
-      Tracker writeTracker = TrackerRepository.getTracker(context(),
-          tester.client.getOutputStream());
-      assertThatTrackerNode(writeTracker)
-          .hasPathStartingWith("Client socket")
-          .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
-              "From " + tester.client.getLocalPort(),
-              FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
-      assertThat(readTracker.twin).isEqualTo(writeTracker);
-      assertThat(writeTracker.twin).isEqualTo(readTracker);
+      testClient(tester);
     }
   }
 
   @Test public void testServerLayered() throws IOException {
     try (SSLSocketTester tester = SSLSocketTester.createLayered()) {
-      Tracker readTracker = TrackerRepository.getTracker(context(), tester.server.getInputStream());
-      assertThatTrackerNode(readTracker)
-          .hasPathStartingWith("Server socket")
-          .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
-              tester.server.getRemoteSocketAddress().toString(),
-              FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
-      Tracker writeTracker = TrackerRepository.getTracker(context(),
-          tester.server.getOutputStream());
-      assertThatTrackerNode(writeTracker)
-          .hasPathStartingWith("Server socket")
-          .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
-              tester.server.getRemoteSocketAddress().toString(),
-              FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
-      assertThat(readTracker.twin).isEqualTo(writeTracker);
-      assertThat(writeTracker.twin).isEqualTo(readTracker);
+      testClient(tester);
     }
+  }
+
+  private void testClient(SSLSocketTester tester) throws IOException {
+    Tracker readTracker = TrackerRepository.getTracker(context(), tester.client.getInputStream());
+    assertThatTrackerNode(readTracker)
+        .hasPathStartingWith("Client socket")
+        .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
+            "From " + tester.client.getLocalPort(),
+            FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
+    Tracker writeTracker = TrackerRepository.getTracker(context(),
+        tester.client.getOutputStream());
+    assertThatTrackerNode(writeTracker)
+        .hasPathStartingWith("Client socket")
+        .hasPathEndingWith(tester.client.getRemoteSocketAddress().toString(),
+            "From " + tester.client.getLocalPort(),
+            FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
+    assertThat(readTracker.twin).isEqualTo(writeTracker);
+    assertThat(writeTracker.twin).isEqualTo(readTracker);
+  }
+
+  private void testServer(SSLSocketTester tester) throws IOException {
+    Tracker readTracker = TrackerRepository.getTracker(context(), tester.server.getInputStream());
+    assertThatTrackerNode(readTracker)
+        .hasPathStartingWith("Server socket")
+        .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
+            tester.server.getRemoteSocketAddress().toString(),
+            FileDescriptorTrackerRepository.READ, SSLSocketImplHook.SSL);
+    Tracker writeTracker = TrackerRepository.getTracker(context(),
+        tester.server.getOutputStream());
+    assertThatTrackerNode(writeTracker)
+        .hasPathStartingWith("Server socket")
+        .hasPathEndingWith(Integer.toString(tester.server.getLocalPort()),
+            tester.server.getRemoteSocketAddress().toString(),
+            FileDescriptorTrackerRepository.WRITE, SSLSocketImplHook.SSL);
+    assertThat(readTracker.twin).isEqualTo(writeTracker);
+    assertThat(writeTracker.twin).isEqualTo(readTracker);
   }
 }
